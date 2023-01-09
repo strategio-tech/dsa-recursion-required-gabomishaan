b1203b1ae22f68c06341d89e97dac4d90e9f9bae
Gabo <mishaan.gabo@gmail.com>
2023-01-09 09:08:26 -0500

    feat: Fib using recursion

--- src/main/java/tech/strategio/E01.java
+++ src/main/java/tech/strategio/E01.java
@@ -4,7 +4,9 @@ import java.util.*;
 
 public class E01 {
     public int getNthFib(int n) {
-        // FIXME
-        return -1;
+        if(n<=1){
+            return n;
+        }
+        return getNthFib(n-1)+ getNthFib(n-2);
     }
 }
