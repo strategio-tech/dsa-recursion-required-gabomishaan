f30ff4ce5ce8332edb2274ac5c46918b0ef1fff6
Gabo <mishaan.gabo@gmail.com>
2023-01-09 11:45:04 -0500

    fix: updated fibonacci solution

--- src/main/java/tech/strategio/E01.java
+++ src/main/java/tech/strategio/E01.java
@@ -5,8 +5,11 @@ import java.util.*;
 public class E01 {
     public int getNthFib(int n) {
         if(n<=1){
-            return n;
+            return 0;
         }
-        return getNthFib(n-1)+ getNthFib(n-2);
+        if(n==2){
+            return 1;
+        }
+        return getNthFib(n-1) + getNthFib(n-2);
     }
 }
